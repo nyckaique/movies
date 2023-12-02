26965b46bf360c1efa6ead9bb9ce368edb52278b
Nycollas Kaique <nycollaskaique@hotmail.com>
2023-12-02 11:01:09 -0300

    att deploy

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -8,9 +8,9 @@ import Upcoming from "../pages/Upcoming";
 export default function Router() {
   return (
     <div>
-      <HashRouter basename="/movies">
+      <HashRouter>
         <Routes>
-          <Route exact path="/movies" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/popular" element={<Popular />} />
           <Route path="/toprated" element={<Toprated />} />
           <Route path="/upcoming" element={<Upcoming />} />
